5d0383d2332f319753dcc93b30f64a5e3644a03e
Derek Lawless <dlawless@gmail.com>
2015-12-27 21:26:25 -0600

    Initial move to angular2 and typescript

---
+++ app/app.component.ts
@@ -0,0 +1,7 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'vaxtrax',
+    template: '<h1>Welcome to Vaxtrax!</h1>'
+})
+export class AppComponent { }
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/app'));
+app.use(express.static(__dirname + '/static'));
 
 app.listen(8080);
