74e706a1a754b2fc649f3aa15a19baf40311bd5f
Derek Lawless <dlawless@gmail.com>
2016-01-23 11:49:59 -0600

    Get the google login button displaying and basically working

--- app/components/welcome/welcome.component.ts
+++ app/components/welcome/welcome.component.ts
@@ -1,18 +1,43 @@
-import {Component} from 'angular2/core';
+/// <reference path="../../../typings/gapi.auth2/gapi.auth2.d.ts" />
+import {Component, AfterContentInit} from 'angular2/core';
 import {Router} from 'angular2/router';
 
 @Component({
   selector: 'vaxtrax-welcome',
   templateUrl: 'app/views/vaxtrax/welcome.html'
 })
-export class WelcomeComponent {
-  constructor(
-    private _router: Router
-  ) { }
+export class WelcomeComponent implements AfterContentInit {
 
-  public onGoogleSignin(googleUser: any): void {
+  public onGoogleSignin = (googleUser: gapi.auth2.GoogleUser): void => {
     // todo
-    this._router.navigate(['Home', {id: 1}]);
+    console.log('I am ' + googleUser.getBasicProfile().getName());
+  };
+
+  private _router: Router;
+  constructor(router: Router) {
+    this._router = router;
+  }
+
+
+  public ngAfterContentInit(): void {
+    this.onRender();
+  }
+
+  public onFailure(): void {
+    // tod
+    console.log('in onFailure');
+  }
+
+  public onRender(): void {
+    gapi.signin2.render('my-signin2', {
+        'scope': 'https://www.googleapis.com/auth/plus.login',
+        'width': 250,
+        'height': 50,
+        'longtitle': true,
+        'theme': 'dark',
+        'onsuccess': this.onGoogleSignin,
+        'onfailure': this.onFailure
+      });
   }
 
   public onStartClick(): void {
